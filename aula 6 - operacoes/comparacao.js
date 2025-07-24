9c9291162b7d67cb6dd52b450dabe667cdfa09a3
Kunst <melissa.kunst@sap.com>
2025-07-24 15:33:12 -0300

    aula 6 - operacoes

---
+++ aula 6 - operacoes/comparacao.js
@@ -0,0 +1,10 @@
+console.log(5 > 3)    //true, 5 é maior que 3
+
+console.log(10 == 5)  //false, 10 não é igual a 5
+
+console.log(!(5 < 10))    //5 é menor que 10, mas fica false pq inverte a operação
+
+console.log((6 % 2 == 0) && (6 > 3))
+//6%2 é 0, então 0 == 0 é true
+//6 é maior que 3, então é true
+//true && true é igual a true
