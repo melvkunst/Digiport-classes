eb7133a6a6e39c746b4f213ddfbbaf6eeff45784
Kunst <melissa.kunst@sap.com>
2025-08-14 14:42:43 -0300

    aula 12 - listas

---
+++ aula 12 - listas/exemplo1.js
@@ -0,0 +1,6 @@
+let programmingLanguager=["JavaScript", "Python", "Java"]
+
+programmingLanguager[1]="TypeScript" //changes
+programmingLanguager.push("Go") //adds
+
+console.log(programmingLanguager)
---
+++ aula 12 - listas/myArrays.js
@@ -0,0 +1,19 @@
+// Arrays are a way to store multiple values in a single variable.
+// They can hold any type of data, including numbers, strings, and even other arrays.
+
+let thingsILike = ["Pizza", "Movies", "Music"];
+
+// There are many methods to manipulate arrays.
+// Access w3schools for more information: https://www.w3schools.com/js/js_array_methods.asp
+
+console.log(thingsILike.length)
+thingsILike.pop()
+thingsILike.pop()
+console.log(thingsILike)
+thingsILike.push("Books")
+thingsILike.push("Music")
+console.log(thingsILike)
+thingsILike.shift()
+console.log(thingsILike)
+thingsILike.unshift("Taylor Swift")
+console.log(thingsILike)
