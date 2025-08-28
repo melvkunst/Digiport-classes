94b3bf9c5464d392dd50d64eab59b9e9066b5bab
Kunst <melissa.kunst@sap.com>
2025-08-28 15:26:17 -0300

    aula 16 - paradigmas

---
+++ aula 15 - funcoes - pratica/listaDeCompras.js
@@ -0,0 +1,26 @@
+let listaDeCompras = [
+    {
+        nome: 'Banana',
+        qty: 5
+    },
+    {
+        nome: 'Maçã',
+        qty: 8
+    }
+]
+
+function adicionarItem(nome, qty){
+    
+}
+
+function removerItem(item){
+
+}
+
+function visualizarLista(){
+
+}
+
+function menu(){
+
+}
---
+++ aula 16 - paradigmas/exemploClassPrivate.js
@@ -0,0 +1,17 @@
+//Paradigmas e Modelagem
+class ContaBancaria {
+    #saldo = 0; //atributo privado
+    depositar(valor){
+        this.#saldo += valor ;
+    }
+    verSaldo(){
+        console.log(`Saldo: R$ ${this.#saldo}`);
+    }
+   
+}
+ 
+ const conta = new ContaBancaria ();
+ conta.depositar(10000000);
+ conta.verSaldo();
+ 
+ 
